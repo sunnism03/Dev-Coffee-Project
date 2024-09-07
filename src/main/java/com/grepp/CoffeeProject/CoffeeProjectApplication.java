da1b69329ea3477d6f61bdf46cb76517566ddf01
Lunabell03 <sunnism03@gmail.com>
2024-09-07 15:29:30 +0900

    [fix] CoffeeProjectApplication에 @EnableJpaAuditing 추가해서 createdAt, updatedAt 저장되도록

--- src/main/java/com/grepp/CoffeeProject/CoffeeProjectApplication.java
+++ src/main/java/com/grepp/CoffeeProject/CoffeeProjectApplication.java
@@ -2,8 +2,10 @@ package com.grepp.CoffeeProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class CoffeeProjectApplication {
 
 	public static void main(String[] args) {
